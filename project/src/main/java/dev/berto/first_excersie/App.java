2be6a7dee98f46c82283494a38c5d0edda7d4f25
berto9675 <albrtvd@gmail.com>
2025-03-21 16:38:32 +0100

    Saving local changes before merging

--- project/src/main/java/dev/berto/first_excersie/App.java
+++ project/src/main/java/dev/berto/first_excersie/App.java
@@ -7,30 +7,5 @@ public final class App {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ingrese tu nombre: ");
-        String primerNombre = scanner.nextLine();
-
-        System.out.println("Ingrese tu apellido: ");
-        String primerApellido = scanner.nextLine();
-
-        System.out.println("Ingrese tu número de edad: ");
-        String numeroDocumento = scanner.nextLine();
-
-        int fechaCumpleaños;
-        while (true) {
-            System.out.println("¿Cuál es tu año de nacimiento? ");
-            fechaCumpleaños = scanner.nextInt();
-            if (fechaCumpleaños > 1900 && fechaCumpleaños < 2022) {
-                break;
-            }
-            System.out.println("Por favor, ingrese un año válido.");
-        }
-
-        Persona persona = new Persona(primerNombre, primerApellido, numeroDocumento, fechaCumpleaños);
-        System.out.println(persona);
-
-        scanner.close();
-    }
+      
 }
